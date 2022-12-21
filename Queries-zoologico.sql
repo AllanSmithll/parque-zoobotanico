e3bd02185678964c3f1b105e3e6161cd7bcd3df5
AllaSmithll <allanammancio2004@gmail.com>
2022-12-20 23:38:29 -0300

    Inserções e queries

---
+++ Queries-zoologico.sql
@@ -0,0 +1,8 @@
+/* 
+1. Procure, na tabela visitante, quando nasceu os visitantes mai novo e o mais velho
+*/
+
+SELECT min(idnasc), max(idnasc)
+FROM VISITANTE;
+
+/* 2. */
--- queries.sql
+++
@@ -1,5 +0,0 @@
-/* 
-1. Procure, na tabela visitante, quais são 5 visitantes mais novos, e os 5 mais velhos. 
-*/
-
-SELECT MIN(i)
